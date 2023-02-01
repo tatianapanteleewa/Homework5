d63e85b3986ee3401de2655a26684ef10b2d6518
tatianapanteleewa <tanyushka5812.efimova@yandex.ru>
2023-02-01 16:15:43 +0300

    without 7

--- index.js
+++ index.js
@@ -84,23 +84,3 @@ function cube() {
 }
 
 cube();
-
-//Задание 7
-
-let month = Number(prompt('Введите номер месяца'));
-
-function season() {
-    if (month === 1 || month === 2 || month === 12) {
-        alert('Время года - Зима');
-    } else if (month === 3 || month === 4 || month === 5) {
-        alert('Время года - Весна');
-    } else if (month === 6 || month === 7 || month === 8) {
-        alert('Время года - Лето');
-    } else if (month === 9 || month === 10 || month === 11) {
-        alert('Время года - Осень');
-    } else {
-        alert('Введено некорректное значение')
-    }
-}
-
-season();

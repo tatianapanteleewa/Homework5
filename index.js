8a394f5d21284b25d5c07dc0a788f6fd7bac0b01
tatianapanteleewa <tanyushka5812.efimova@yandex.ru>
2023-02-03 11:44:27 +0300

    changes1

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ console.log(lessNumber(8, 8));
 
 //Задание 2
 
-let userNumber = Number(prompt('Введите число'));
-
-function evenNumber() {
+function evenNumber(userNumber) {
     if (userNumber % 2 == 0) {
         return `Число четное`;
     } else {
@@ -24,24 +22,27 @@ function evenNumber() {
 
 }
 
-evenNumber();
+evenNumber(7);
 
 //Задание 3
 
-let userNum = Number(prompt('Введите число'));
-
-function squareNumber() {
-    console.log(userNum ** 2);
-    return userNum ** 2;
+function squareNumber1(num) {
+    console.log(num ** 2);
 }
 
-squareNumber();
+function squareNumber2(num) {
+    return num ** 2;
+}
+
+squareNumber1(3);
+console.log(squareNumber2(4));
 
 //Задание 4
 
-let userAge = Number(prompt('Сколько вам лет?'));
-
 function age() {
+
+    let userAge = Number(prompt('Сколько вам лет?'));
+
     if (userAge < 0) {
         alert("Вы ввели неправильное значение");
     } else if (userAge > 0 && userAge < 12) {
@@ -56,30 +57,26 @@ age();
 
 //Задание 5
 
-let userAnswer1 = prompt('Введите число №1');
-let userAnswer2 = prompt('Введите число №2');
-
-function ifNumber() {
+function ifNumber(userAnswer1, userAnswer2) {
     if (Number.isNaN(userAnswer1) === true || Number.isNaN(userAnswer2) === true) {
-        alert("Одно или оба значения не являются числом");
+        return ("Одно или оба значения не являются числом");
     } else {
-        alert(userAnswer1 * userAnswer2);
+        return (userAnswer1 * userAnswer2);
     }
 
 }
 
-ifNumber();
+ifNumber(3, 5);
 
 //Задание 6
 
-let userN = prompt('Введите число');
-
 function cube() {
+    let userN = prompt('Введите число');
     if (isNaN(userN) === false) {
         let numberCube = userN ** 3;
-        alert(`n в кубе равняется ${numberCube}`);
+        return (`n в кубе равняется ${numberCube}`);
     } else {
-        alert('Переданный параметр не является числом');
+        return ('Переданный параметр не является числом');
     }
 }
 
